8ea1628da1f34930c05298e4e356f8c31c4d8818
Edson Ferreira <45494938+brasilsites@users.noreply.github.com>
2019-05-26 20:03:16 -0300

    Update upload-em-c.c

--- upload-em-c.c
+++ upload-em-c.c
@@ -4,13 +4,14 @@
 
 int main(void) {
   void copiaConteudo(FILE *arquivo, FILE *arquivo1);
+//Crie a mesma pasta e salve o arquivo com este mesmo nome
   FILE *arquivo = fopen("tmp/curriculum.txt","r");
 if (arquivo == NULL)
 {
     printf ("Não foi possível abrir o arquivo");
     return 1;
 }
-
+///O programa cria um arquivo com o nome que escolher, se já tiver um arquivo com o mesmo nome será apagado
 FILE *arquivo1 = fopen("home/20192024.txt","w+");
 
 copiaConteudo(arquivo,arquivo1);
